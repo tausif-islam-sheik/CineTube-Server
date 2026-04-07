29cb735d8935f0fddec483ec202f8d627be230b8
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-04-08 01:11:03 +0600

    feat: Configure Environment Variables

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -4,5 +4,6 @@ import { AuthController } from "./auth.controller";
 const router = Router();
 
 router.post("/register", AuthController.register);
+router.post("/login", AuthController.logIn);
 
 export const AuthRoutes = router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from "./app";
-// import { env } from "./app/config/env";
+import { envVars } from "./app/config/env";
 
 const bootstrap = async () => {
   try {
-    app.listen(5000, () => {
-      console.log(`Server running on port http://localhost:5000`);
+    app.listen(envVars.PORT, () => {
+      console.log(`Server running on port http://localhost:${envVars.PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
